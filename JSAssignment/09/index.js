f0d3391b65947de2a3f46a39c0a2021a93f57384
deepoa <deepeaaa45@gmail.com>
2023-05-07 11:51:39 +0530

    js assignment completed

---
+++ JSAssignment/09/index.js
@@ -0,0 +1,60 @@
+// 09. Boolean value is either true or false.
+//     - Write three JavaScript statement example which provide truthy value.
+//     - Write three JavaScript statement example which provide falsy value.
+
+//truth value :
+//first case- 1 :Here if value will be executed because 1 will have true value.
+if(1)
+{
+    console.log('if block will be executed');
+}
+else
+{
+   console.log('else block will be executed ');
+}
+//second case-kapil :Here if value will be executed 1 will have true value.
+if('Kapil')
+{
+    console.log('if block will be executed');
+}
+else
+{
+    console.log('else block will be executed');
+}
+//third case-{} : Here if value will be executed
+if({})
+{
+    console.log('if block will be executed');
+}
+else
+{
+    console.log('else block will be executed');
+}
+
+
+
+//false value :
+//first case-null value :Here else condition will be executed because if condition will have null value 
+if(null){
+    console.log('if block will be executed');
+}
+else{
+    console.log('else block will be executed');
+}
+//second case=0 :Here else condition will be executed because if condition will have 0 value
+if(0)
+{
+    console.log('If value will be executed');
+}
+else{
+    console.log('else block will be executed');
+}
+//third case=NaN :Here else condition will be executed because if condition will have NaN value
+if(NaN)
+{
+    console.log('If value will be executed');
+}
+else
+{
+    console.log('else block will be executed');
+}
---
+++ JSAssignment/readme.md
@@ -0,0 +1,5 @@
+**JAVA SCRIPT ASSIGNMENT**
+
+
+![IMG](https://img.shields.io/badge/JAVASCRIPT-ASSIGNMENT-green)
+[LINK](https://github.com/kapilsarkar/FSJS2.0/tree/main/JAVASCRIPT%20ASSIGNMENTS)
